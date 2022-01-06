4c84d2c3a60f839da3f3b42e9f8667d7a6008e6b
Anusha211209 <84378742+Anusha211209@users.noreply.github.com>
2022-01-06 22:26:59 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAep2M-CLanZ0yZDRj5FN1Vrw8pb1p7MmY",
+    authDomain: "letschat-55ee7.firebaseapp.com",
+    projectId: "letschat-55ee7",
+    storageBucket: "letschat-55ee7.appspot.com",
+    messagingSenderId: "1023919834357",
+    appId: "1:1023919834357:web:41a057a35e93b06d5ff1bc",
+    measurementId: "G-E3K60X2JTT"
+};
+
